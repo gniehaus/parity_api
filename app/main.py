754a6bccbe5d8706e214869494ef85211e4a89db
grayson niehaus <graysonniehaus@gmail.com>
2026-07-17 23:19:53 -0400

    removing plaid investments

--- app/main.py
+++ app/main.py
@@ -30,7 +30,6 @@ from .plaid_service import (
     exchange_public_token,
     sync_bank_accounts,
     get_bank_accounts_from_db,
-    test_plaid_investments,
 )
 from .portfolio_dashboard_engine import calculate_portfolio_dashboard
 
